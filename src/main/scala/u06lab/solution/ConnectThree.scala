7e28ffc97343d1b903859f8df3c018b3e542e44c
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-04-14 15:44:03 +0200

    ex 3 first version

--- src/main/scala/u06lab/solution/ConnectThree.scala
+++ src/main/scala/u06lab/solution/ConnectThree.scala
@@ -5,11 +5,12 @@ import java.util.OptionalInt
 object ConnectThree extends App:
   val bound = 3
   enum Player:
-    case X, O
-    def other: Player = this match
+    case X, O // Two player: X and O
+    def other: Player = this match // Return the "id" of the other player in the match
       case X => O
       case _ => X
 
+  // Represent the place of one disk associated to the player that left it
   case class Disk(x: Int, y: Int, player: Player)
   /**
    * Board:
@@ -26,13 +27,32 @@ object ConnectThree extends App:
 
   import Player.*
 
-  def find(board: Board, x: Int, y: Int): Option[Player] = ???
+  def find(board: Board, x: Int, y: Int): Option[Player] = board.find(d => d.x == x && d.y == y).map(_.player)
 
-  def firstAvailableRow(board: Board, x: Int): Option[Int] = ???
+  def firstAvailableRow(board: Board, x: Int): Option[Int] =
+    val pos = (0 to bound).diff(board.filter(_.x == x).map(_.y));
+    Option.when(!pos.isEmpty)(pos.min)
 
-  def placeAnyDisk(board: Board, player: Player): Seq[Board] = ???
+  def placeAnyDisk(board: Board, player: Player): Seq[Board] =
+    for
+      x <- 0 to bound // Iterate columns to choice
+      y = firstAvailableRow(board, x)
+      if !y.isEmpty
+    yield
+      Disk(x, y.get, player) +:board
 
-  def computeAnyGame(player: Player, moves: Int): LazyList[Game] = ???
+  def computeAnyGame(player: Player, moves: Int): LazyList[Game] = (moves match
+    case 0 => LazyList[Game](List(List())) // LazyList[Game](List[Board](List[Disk](Disk, ...), ...), ...)
+    case _ =>
+      for
+        game <- computeAnyGame(player.other, moves - 1)
+        lastBoard = game.head
+        nextBoard <- placeAnyDisk(lastBoard, player).reverse // reverse in order to have the same output sequence as described below
+      yield
+        if !isGameOver(lastBoard) then // if internal in the for won't work, why?
+          nextBoard +: game
+        else
+          game).distinct
 
   def printBoards(game: Seq[Board]): Unit =
     for
@@ -44,6 +64,24 @@ object ConnectThree extends App:
       if x == bound then
         print(" ")
         if board == game.head then println()
+
+  private def isGameOver(board: Board): Boolean =
+      // Inneficient, for educational purposes.
+      var status = false
+      for
+        disk <- board
+        x = disk.x
+        y = disk.y
+        player = disk.player
+        dx <- -1 to 1
+        dy <- -1 to 1
+        if !(dx == 0 && dy == 0)
+        if x + 2 * dx <= bound && y + 2 * dy <= bound
+        if find(board, x + dx, y + dy).filter(_ == player).isDefined &&
+          find(board, x + 2 * dx, y + 2 * dy).filter(_ == player).isDefined
+      do
+        status = true
+      status
 
   // Exercise 1: implement find such that..
   println("EX 1: ")
@@ -71,7 +109,7 @@ object ConnectThree extends App:
   // ...O ..XO .X.O X..O
   println("EX 3: ")
 // Exercise 3 (ADVANCED!): implement computeAnyGame such that..
-  computeAnyGame(O, 4).foreach { g =>
+  computeAnyGame(O, 7).foreach { g =>
     printBoards(g)
     println()
   }
